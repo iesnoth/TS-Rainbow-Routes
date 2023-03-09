c380a40f0e09ab22dd988e3dc461bc6dd4f774ba
Victoria Beasley <iesnoth91@gmail.com>
2023-03-08 19:44:47 -0600

    finished activity

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 require('dotenv').config()
 
 // Require needed node modules
-const express = require('express')
+import express from 'express'
 
 // Initialize your application by calling the function returned by the express module
 const app = express()
@@ -10,7 +10,8 @@ const app = express()
 // Declare routes that people can visit on the application
 
 // White Page
-app.get('/', function (req, res) {
+//express.Request/Response calls interfaces from the express API
+app.get('/', function (req: express.Request, res: express.Response): void {
     res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: white;">
@@ -23,9 +24,11 @@ app.get('/', function (req, res) {
 
 
 // Color Page
-app.get('/:color', function (req, res) {
-    let myColor = req.params.color
-    res.send(`
+const app.get('/:color', function (req: express.Request, res: express.Response) {
+    //string methods don't work on type 'unkown'
+    let myColor: unknown = req.params.color
+    if (typeof (myColor) === 'string') {
+        res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
                 <h2 style="text-align: center;">NAV BAR</h2>
@@ -33,6 +36,7 @@ app.get('/:color', function (req, res) {
             <h1 style="color: ${myColor};">${myColor.charAt(0).toUpperCase() + myColor.slice(1)} Page</h1>
         </body>
     `)
+    }
 })
 
 
